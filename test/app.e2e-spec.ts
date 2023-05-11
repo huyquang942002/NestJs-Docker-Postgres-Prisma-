6ff59485b5ca9c77bedb825a3a253ec4fbadcc25
huyquang <huywocker92016@gmail.com>
2023-05-11 16:52:03 +0700

    ignore unit test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,12 +1,16 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
+import * as pactum from 'pactum';
 import {
   INestApplication,
   ValidationPipe,
 } from '@nestjs/common';
+import { PrismaService } from '../src/prisma/prisma.service';
 
 describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
+
   beforeAll(async () => {
     const moduleRef =
       await Test.createTestingModule({
@@ -19,9 +23,31 @@ describe('App e2e', () => {
       }),
     );
     await app.init();
+    app.listen(3333);
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
   });
-  afterAll(() => {
-    app.close();
+  afterAll(() => {});
+  describe('Auth', () => {
+    describe('SignUp', () => {
+      it.todo('Should signup');
+    });
+
+    describe('SignIn', () => {});
   });
-  it.todo('should pass');
+  describe('User', () => {
+    describe('Get me', () => {});
+
+    describe('Edit User', () => {});
+  });
+  describe('BookMarks', () => {
+    describe('Create BookMarks', () => {});
+
+    describe('Get BookMarks', () => {});
+    describe('Get BookMarks by id', () => {});
+
+    describe('Edit BookMarks', () => {});
+
+    describe('Delete BookMarks', () => {});
+  });
 });
